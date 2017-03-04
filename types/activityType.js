9b3d1dffb4bd7f9a44d9c6ff6b372761aacc5e19
Daniel Iglesias <diglesias17@hotmail.com>
2017-03-04 19:32:09 -0300

    Trackers files removed

---
+++ types/activityType.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var ActivityType = {
+	None: -1,
+	Idle: 0,
+	System: 1,
+	Coding: 2,
+	Debugging: 3,
+	Navigating: 4,
+	Building: 5,
+	
+	Event: 1000,
+
+	DocumentFocus: 1001,
+	DocumentEdit: 1002,
+
+	OpenSolution: 1003,
+	CloseSolution: 1004,
+	BuildSolutionFailed: 1005,
+	BuildSolutionSucceded: 1006,
+	BuildSolutionCancelled: 1007,
+
+	BuildProject: 1008,
+	BuildProjectFailed: 1009,
+	BuildProjectSucceeded: 1010,
+	BuildProjectCancelled: 1011
+}
+
+module.exports = { ActivityType };
